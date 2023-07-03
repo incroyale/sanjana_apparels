c2716f931cc8c131b64a8b9a6e2de7706333bdba
incroyale <gandhideep2003@gmail.com>
2023-07-03 23:16:55 +0530

    completed delivery dates, without a space

--- delivery_date.py
+++ delivery_date.py
@@ -1,13 +1,178 @@
 from database import create_connection
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime, timedelta
 
 cnx = create_connection()
 if cnx is not None:
     cursor = cnx.cursor()
 
 
-def display_deliver_date(root):
+# Input Date Format : "6/6/23"
+def display_delivery_date(root):
     date_window = tk.Toplevel(root)
-    date_window.title("Suit Search")
+    date_window.title("Delivery Date Display")
     date_window.geometry("1000x1000")
+
+    # Fetch Dates
+    cursor.execute("SELECT ddate FROM suit;")
+    suit_rows = cursor.fetchall()
+    cursor.execute("SELECT ddate FROM blouse;")
+    blouse_rows = cursor.fetchall()
+
+    # Convert Dates To DD/MM/YYYY -> 31/12/2015
+    d3 = []
+    d2 = []
+    d1 = []
+    d0 = []
+
+    for i in suit_rows:
+        if '/' in i[0]:
+            ddate = i[0].split('/')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+        elif '-' in i[0]:
+            ddate = i[0].split('-')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+    for i in blouse_rows:
+        if '/' in i[0]:
+            ddate = i[0].split('/')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+        elif '-' in i[0]:
+            ddate = i[0].split('-')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+    d0 = [*set(d0)]
+    d1 = [*set(d1)]
+    d2 = [*set(d2)]
+    d3 = [*set(d3)]
+
+    label = tk.Label(date_window, text="Upcoming Orders", bg='#E6E6FA', fg='black',
+                     font=('calibri', 24, 'bold'))
+    label.pack(side=tk.LEFT)
+
+    date_frame = ttk.Frame(date_window)
+    date_frame.pack(fill=tk.BOTH, expand=True)
+    date_scroll_y = ttk.Scrollbar(date_frame, orient=tk.VERTICAL)
+    date_scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
+    date_tree = ttk.Treeview(date_frame, style='Treeview', yscrollcommand=date_scroll_y)
+    date_tree.pack(fill=tk.BOTH, expand=True)
+    date_scroll_y.configure(command=date_tree.yview)
+
+    columns = ('Name', 'Bill No.', 'Date')
+    column_widths = (300, 300, 300)
+    date_tree['columns'] = columns
+    date_tree.column('#0', width=0, stretch=tk.NO)
+
+    for col, width in zip(columns, column_widths):
+        date_tree.column(col, width=width)
+        date_tree.heading(col, text=col, anchor=tk.CENTER)
+
+    # --------------------------------------------------------
+    for i in date_tree.get_children():
+        date_tree.delete(i)
+
+    for i in d0:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        s_rows = cursor.fetchall()
+        for j in s_rows:
+            date_tree.insert('', tk.END, values=j)
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(squery, (i,))
+        b_rows = cursor.fetchall()
+        for j in b_rows:
+            date_tree.insert('', tk.END, values=j)
+
+    for i in d1:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        s_rows = cursor.fetchall()
+        for j in s_rows:
+            date_tree.insert('', tk.END, values=j)
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(squery, (i,))
+        b_rows = cursor.fetchall()
+        for j in b_rows:
+            date_tree.insert('', tk.END, values=j)
+
+    for i in d2:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        s_rows = cursor.fetchall()
+        for j in s_rows:
+            date_tree.insert('', tk.END, values=j)
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(squery, (i,))
+        b_rows = cursor.fetchall()
+        for j in b_rows:
+            date_tree.insert('', tk.END, values=j)
+
+    for i in d3:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        s_rows = cursor.fetchall()
+        for j in s_rows:
+            date_tree.insert('', tk.END, values=j)
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(squery, (i,))
+        b_rows = cursor.fetchall()
+        for j in b_rows:
+            date_tree.insert('', tk.END, values=j)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from database import create_connection
+from datetime import datetime, timedelta
+
 
 global custom_font, search_entry, suit_window
 
@@ -27,10 +29,8 @@ def search_button():
     global tree, blouse_tree
 
     suit_window = tk.Toplevel(root)
-    suit_window.title("Suit Search")
+    suit_window.title("Search by Name")
     suit_window.geometry("1000x1000")
-    scroll_y = ttk.Scrollbar(suit_window, orient=tk.VERTICAL)
-    scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
 
     # Create a frame for the top row elements
     top_frame = ttk.Frame(suit_window)
@@ -88,7 +88,8 @@ def search_button():
                'Front Neck', 'Back Neck', 'Arm Hole', 'Pant', 'Pant Length', 'Waist', 'Thigh', 'Bottom',
                'Note', 'Delivery Date')
 
-    blouse_columns = ('Name', 'Phone', 'Date', 'Bill No.', 'Length', 'Shoulder', 'Arm Hole', 'Sleeves', 'Size', 'T',
+# Check with other Database
+    blouse_columns = ('Name', 'Date', 'Bill No.', 'Phone', 'Length', 'Shoulder', 'Arm Hole', 'Sleeves', 'Size', 'T',
                       'Front Neck', 'Back Neck', 'Note', 'Delivery Date')
 
     column_widths = (150, 200, 200, 200, 200, 200, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 500, 150)
@@ -143,10 +144,206 @@ def name_search():
     blouse_tree.tag_configure('odd_row', background='#FFFFFF', foreground='black')
     blouse_tree.tag_configure('row_font', font=('Arial', 12))
 
+def display_delivery_date():
+    date_window = tk.Toplevel(root)
+    date_window.title("Delivery Date Display")
+    date_window.geometry("1000x1000")
+
+    # Fetch Dates
+    cursor.execute("SELECT ddate FROM suit;")
+    suit_rows = cursor.fetchall()
+    cursor.execute("SELECT ddate FROM blouse;")
+    blouse_rows = cursor.fetchall()
+
+    # Convert Dates To DD/MM/YYYY -> 31/12/2015
+    d3 = []
+    d2 = []
+    d1 = []
+    d0 = []
+
+    for i in suit_rows:
+        if '/' in i[0]:
+            ddate = i[0].split('/')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+        elif '-' in i[0]:
+            ddate = i[0].split('-')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+    for i in blouse_rows:
+        if '/' in i[0]:
+            ddate = i[0].split('/')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+        elif '-' in i[0]:
+            ddate = i[0].split('-')
+            if len(ddate[2]) == 2:
+                ddate[2] = int('20' + ddate[2])
+            ddate[0] = int(ddate[0])
+            ddate[1] = int(ddate[1])
+            a = datetime(ddate[2], ddate[1], ddate[0])
+            # Delivery classified by number of days
+            if timedelta(days=3) > a - datetime.now() > timedelta(days=2):
+                d3.append(i[0])
+            elif timedelta(days=2) > a - datetime.now() > timedelta(days=1):
+                d2.append(i[0])
+            elif timedelta(days=1) > a - datetime.now() > timedelta(days=0):
+                d1.append(i[0])
+            elif a - datetime.now() < timedelta(0):
+                d0.append(i[0])
+
+    d0 = [*set(d0)]
+    d1 = [*set(d1)]
+    d2 = [*set(d2)]
+    d3 = [*set(d3)]
+
+    label = tk.Label(date_window, text="Upcoming Orders", bg='#E6E6FA', fg='black',
+                     font=('calibri', 24, 'bold'))
+    label.pack(side=tk.LEFT)
+
+    date_frame = ttk.Frame(date_window)
+    date_frame.pack(fill=tk.BOTH, expand=True)
+    date_scroll_y = ttk.Scrollbar(date_frame, orient=tk.VERTICAL)
+    date_scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
+    date_tree = ttk.Treeview(date_frame, style='Treeview', yscrollcommand=date_scroll_y)
+    date_tree.pack(fill=tk.BOTH, expand=True)
+    date_scroll_y.configure(command=date_tree.yview)
+
+    columns = ('Name', 'Bill No.', 'Date')
+    column_widths = (300, 300, 300)
+    date_tree['columns'] = columns
+    date_tree.column('#0', width=0, stretch=tk.NO)
+
+    for col, width in zip(columns, column_widths):
+        date_tree.column(col, width=width)
+        date_tree.heading(col, text=col, anchor=tk.CENTER)
+
+    for i in date_tree.get_children():
+        date_tree.delete(i)
+
+    for i in d0:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        srows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(bquery, (i,))
+        brows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+
+    for i in d1:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        srows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(bquery, (i,))
+        brows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+
+    for i in d2:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        srows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(bquery, (i,))
+        brows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+
+    for i in d3:
+        squery = "SELECT name, billno, ddate FROM suit where ddate = %s"
+        cursor.execute(squery, (i,))
+        srows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+        bquery = "SELECT name, billno, ddate FROM blouse where ddate = %s"
+        cursor.execute(bquery, (i,))
+        brows = cursor.fetchall()
+        for j, result in enumerate(srows):
+            if j % 2 == 0:
+                date_tree.insert('', tk.END, values=result, tags=('even_row', 'row_font'))
+            else:
+                date_tree.insert('', tk.END, values=result, tags=('odd_row', 'row_font'))
+
+    date_tree.tag_configure('even_row', background='#F0F0F0', foreground='black')
+    date_tree.tag_configure('odd_row', background='#FFFFFF', foreground='black')
+    date_tree.tag_configure('row_font', font=('Arial', 14))
+
 
 suit_button = tk.Button(root, text="Search by Name", bg='#3498DB', fg='white', command=search_button,
                         font=("Arial", 14), padx=20, pady=7)
 suit_button.configure(bg='#3498DB')
 suit_button.grid(row=6, column=8, columnspan=3, padx=50, pady=7, rowspan=3)
 
+date_button = tk.Button(root, text="Show Upcoming Deliveries", bg='#3498DB', fg='white', command=display_delivery_date,
+                        font=("Arial", 14), padx=20, pady=7)
+date_button.configure(bg='#3498DB')
+date_button.grid(row=8, column=8, columnspan=3, padx=50, pady=7, rowspan=3)
 root.mainloop()
